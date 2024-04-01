b963d98cd54a730ce0ab829a4fad77f56358e822
AntonyVilanovaSilva <115712032+AntonyVilanovaSilva@users.noreply.github.com>
2024-04-01 12:20:25 -0300

    01/04

---
+++ Frontend/helloap/src/components/ButtonPrincipal.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './ButtonPrincipal'
+function ButtonPrincipal() {
+       return(
+        <div className="btnEnviarEstilo">Enviar</div>
+       )
+       
+}
+
+export default ButtonPrincipal
